8935de6efff0c01bf0482d111359831468fc4691
brad <bradtaniguchi@gmail.com>
2017-08-30 22:44:20 -0700

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,11 @@ const paths = {
 gulp.task('build', () => {
   gulp.src([
     paths.src + '/**/*.js',
-    paths.lib + '/angular/angular.js'  
+    paths.lib + '/angular/angular.js'
   ]).pipe(gulp.dest(paths.dest + '/js')); 
+  gulp.src([
+    paths.src + '/index.html'
+  ]).pipe(gulp.dest(paths.dest));
 });
 gulp.task('default', ['build']);
 
---
+++ src/js/app.controller.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+  angular.module('app')
+  .controller('AppController', AppController);
+  AppController.$inject=['$log'];
+    
+  function AppController($log) {
+    var vm = this;
+    vm.$onInit = onInit;
+    return vm;
+    
+    function onInit() {
+      
+    }
+  }
+})();
