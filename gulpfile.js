febd3fd21ac4275a28b2a1d56db74d25424db590
brad <bradtaniguchi@gmail.com>
2017-09-02 21:55:37 -0700

    Added live-server support
    Now npm start, or gulp start, starts both the gulp watch command and
    the live-server utility.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,13 @@
 const gulp = require('gulp');
+const server = require('live-server');
 const paths = {
   src: './src',
   lib: './node_modules',
   dest: 'build'
+};
+const serverParams = {
+  port: 8080,
+  host: '0.0.0.0'
 };
 gulp.task('build', () => {
   gulp.src([
@@ -23,6 +28,9 @@ gulp.task('start', () => {
     paths.src + '/**/*.js',
     paths.src + '/index.html'
   ], ['build']);
+
+  console.log('Starting live-server');
+  server.start(serverParams);
 });
 gulp.task('default', ['build', 'start']);
 
